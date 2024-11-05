910af1114da0430a698c1e9669332c4e18f6b2c7
hicervan <hicervan@ucsc.edu>
2024-11-04 19:11:55 -0800

    fix lab 8 chatgbt

--- lab8/js/lab8.js
+++ lab8/js/lab8.js
@@ -1,28 +1,26 @@
 /**
- * Author:    Hilary
+ * Author:    Hilary Cervantes with the help of chatGBT
  * Created:   10.27.2024
  * 
  * (c) Creative commons
  * **/
 
-//function
-//create a variable array
-let littleG = [1,2,3,4,5]
+// Create a variable array
+let littleG = [1, 2, 3, 4, 5];
 
-//create a function add 100
+// Create a function to add 100
 function add100(n) {
-   return n+100;
+   return n + 100;
 }
 
 console.log(add100(5));
 
-//manipulate your varible array with your function add 100
+// Manipulate the array with the add100 function
 let results = littleG.map(add100);
 console.log(results);
 
-//use an anonymous function to manipulate your array
-littleG.map(function(x) {
-  return x-2; 
-})
-
-console.log(little.G);
+// Use an anonymous function to manipulate the array
+let newResults = littleG.map(function(x) {
+  return x - 2; 
+});
+console.log(newResults);  // Log the new results
